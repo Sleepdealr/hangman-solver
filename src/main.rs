643f0477aa3c71627a978f26a79e49bf2f4fed39
Sleepdealer <dreznik01@gmail.com>
2021-06-13 09:54:00 -0400

    Updated

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use std::{
     path::Path,
 };
 use std::collections::HashMap;
-
-// use regex::Regex;
+use regex::Regex;
 
 fn read_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
@@ -29,13 +28,37 @@ fn get_most_common_letter(list: &mut Vec<String>) -> char {
     mcl
 }
 
-fn main(){
+fn prune_vec(words: &mut Vec<String> , incorrect_letters:&str){
+    let formatted = format!("/[{}]/" , incorrect_letters);
+    let incorrect = Regex::new(&*formatted).unwrap();
+    words.retain(|x|  incorrect.is_match(&x) );
+}
+
+fn game(mut secret_word:String){
+    let (mut incorrect_letters , mut cur_guess , mut attempts, mut guess): (String, String, u8, char) = (String::new(), "_".repeat(secret_word.len()), 0 , ' ');
+
     let mut words: Vec<String> = read_file("english-words/words_lowercase.txt").expect("Could not load lines"); // Loads all lines from file into words
-    println!("What word do you want it to guess?");
-    let mut secret_word = String::new();
-    std::io::stdin().read_line(&mut secret_word).unwrap(); // Gets user input from stdin
-    secret_word.pop(); // Remove \n from word
     words.retain(|x  | x.len() == secret_word.len()); // Prunes all words that aren't the correct length
+
+    loop{ // Game loop
+        guess = get_most_common_letter(&mut words);
+
+        if secret_word.eq(&cur_guess) {
+            break;
+        }
+    }
+
+    prune_vec(&mut words, &*incorrect_letters);
+
     println!("{:?}" , words);
     println!("Most common letter: {}" , get_most_common_letter(&mut words) )
+}
+
+fn main(){
+    let mut secret_word = String::new();
+    println!("What word do you want it to guess?");
+    std::io::stdin().read_line(&mut secret_word).unwrap(); // Gets user input from stdin
+    secret_word = secret_word.trim().to_string(); // Remove \n and other gunk from end of word
+    game(secret_word);
+
 }
