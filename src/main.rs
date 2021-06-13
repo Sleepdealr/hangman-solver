a86ae6b2371fbbdb5c4eeccaa938a5dd8a1d8c31
Sleepdealer <dreznik01@gmail.com>
2021-06-13 05:46:37 -0400

    Updated

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
 use std::{
     fs::File,
-    io::{self, BufRead, BufReader, Read},
+    io::{self, BufRead, BufReader},
     path::Path,
-    sync::Mutex
 };
+use std::collections::HashMap;
+
 // use regex::Regex;
 
-fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
+fn read_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
 
-fn prune_list(){
-    //length
+fn get_most_common_letter(list: &mut Vec<String>) -> char {
+    let mut letter_map:HashMap<char , u32> = HashMap::new();
+    for word in list.into_iter(){
+        for (_index , letter) in word.chars().enumerate(){ // Gets index(unused) and letter of each word
+            *letter_map.entry(letter).or_insert(1) += 1; // Adds letter to map if it doesn't exit
+        }
+    }
+    let mut mcl:char = ' ';
+    let mut mcl_val = 0;
+    for l in letter_map{
+        if l.1 > mcl_val {
+            mcl = l.0;
+            mcl_val = l.1;
+        }
+    }
+    mcl
 }
 
-
-//how to send mutatable vec
-fn asdasd(asd: &mut Vec<String>){
-    asd[0] = "str".parse().unwrap();
-
-}
-
-fn main() {
-    let mut words  = lines_from_file("english-words/words_lowercase.txt").expect("Could not load lines");
-    asdasd(&mut words);
-    println!("{}" , words[0]);
+fn main(){
+    let mut words: Vec<String> = read_file("english-words/words_lowercase.txt").expect("Could not load lines"); // Loads all lines from file into words
+    println!("What word do you want it to guess?");
+    let mut secret_word = String::new();
+    std::io::stdin().read_line(&mut secret_word).unwrap(); // Gets user input from stdin
+    secret_word.pop(); // Remove \n from word
+    words.retain(|x  | x.len() == secret_word.len()); // Prunes all words that aren't the correct length
+    println!("{:?}" , words);
+    println!("Most common letter: {}" , get_most_common_letter(&mut words) )
 }
